476229fc4c53dedd1373fcab1b12fbed61f9dfc4
AgusFassola <agusfassolacarp@gmail.com>
2025-03-14 13:59:01 -0300

    CRUD users funcionando

--- back/server.js
+++ back/server.js
@@ -28,6 +28,32 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model('User', userSchema);
 
+
+//middleware de autenticacion
+const autenticateToken = (req, res, next) =>{
+    const token = req.header('Authorization');
+    if(!token) return res.status(401).json({ message:'No tiene autorizacion' });
+
+    try{
+        const verified = jwt.verify(token.replace('Bearer ',''), process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    }catch(err){
+        res.status(403).json({ message:'Token inválido' });
+        console.log("error de token:", err);
+    }
+};
+
+//Middleware de autorizacion por rol
+const autorizarRole = (roles) =>{
+    return(req, res, next) =>{
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({ message:'Acceso no autorizado' });
+        }
+        next();
+    };
+};
+
 //Registrar un usuario
 app.post('/api/register', async (req, res) => {
 
@@ -51,7 +77,7 @@ app.post('/api/register', async (req, res) => {
 app.post('/api/login', async (req, res) => {
 
     try{
-        const{name, password} = req.body;
+        const{email, password} = req.body;
         const user = await User.findOne({email});
         if(!user) return res.status(400).json({message:'El usuario no existe'});
 
@@ -71,7 +97,61 @@ app.post('/api/login', async (req, res) => {
 
     }catch(error){
         res.status(500).json({ message:'Error al iniciar sesion' });
+        console.log("error al iniciar sesion:",error);
+
     }
+});
+
+//ruta solo para admin
+app.get('/api/users', autenticateToken, autorizarRole(['admin']), async (req,res) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    }catch(err){
+        res.status(500).json({ message:'Error al obtener usuarios' });
+    }
+});
+
+//obtener usuario por id
+app.get('/api/users/:id', autenticateToken, async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user) return res.status(404).json({ message:'Usuario no encontrado' });
+        res.json(user);
+    }catch(err){
+        res.status(500).json({ message:'Error al traer el usuario' });
+    }
+});
+
+//actualizar el usuario
+app.put('/api/users/:id', autenticateToken, async (req,res) => {
+    try{
+        const { name,email,role } = req.body;
+        const updateUser = await User.findByIdAndUpdate(req.params.id,{ name,email,role }, {new:true});
+        console.log("Error llegué acá:");
+        if(!updateUser) return res.status(404).json({ message:'Usuario no encontrado para editar' });
+        console.log("ahora Error llegué acá:");
+        res.json(updateUser);
+    }catch(err){
+        res.status(500).json({ message:'Error en el servidor actualizar' });
+        console.log("Error acá:", err);
+    }
+});
+
+//eliminar un usuario(solo admin)
+app.delete('/api/users/:id', autenticateToken, autorizarRole(['admin']), async (req, res) => {
+    try{
+        const deleteUser = await User.findByIdAndDelete(req.params.id);
+        if(!deleteUser) return res.status(404).json({ message:'Usuario no encontrado' });
+        res.json({ message:'Eliminado con éxito' });
+    }catch(err){
+        res.status(500).json({ message:'Error en el servidor de eliminar' });
+    }
+});
+
+//ruta solo para admin
+app.get('/api/admin', autenticateToken, autorizarRole(['admin']), (req,res)=>{
+    res.json({ message:'Bienvenido admin' });
 });
 
 app.listen(PORT, () =>{
