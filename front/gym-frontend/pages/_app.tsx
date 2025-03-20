1bcd046e9b86e68a05518d95b437305477ce98f7
AgusFassola <agusfassolacarp@gmail.com>
2025-03-20 17:21:30 -0300

    instalacion next y typescript en front

---
+++ front/gym-frontend/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps){
+    return(
+        <>
+            <ToastContainer position='top-right' autoClose={3000} />
+            <Component {...pageProps}/>
+        </>
+    );
+}
